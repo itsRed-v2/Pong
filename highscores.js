76177548d0812a83eb124cd6e4acdcf5094dc4e4
Etienne Charignon <etienne.charignon@gmail.com>
2020-05-24 20:01:27 +0200

    Sauvegarde des scores du serveur

--- highscores.js
+++ highscores.js
@@ -1,19 +1,18 @@
 module.exports = {
-mode_plus: {
-'Nokari' : 107,
-'vrai_itsRed_v2' : 92,
-'nijou_' : 13,
-'Céline Leroy' : 10,
-'Étienne C🌀' : 7,
-'itsRed_v2' : 2,
-},
-mode_moins: {
-'Nokari' : 50,
-'itsRed_v2' : 27,
-},
-mode_double: {
-'Nokari' : 33,
-'Étienne C🌀' : 8,
-'itsRed_v2' : 4,
-},
+    mode_plus: {
+    'Nokari' : 107,
+    'itsRed_v2' : 92,
+    'nijou_' : 13,
+    'Céline Leroy' : 10,
+    'Étienne C🌀' : 7,
+    },
+    mode_moins: {
+    'Nokari' : 50,
+    'itsRed_v2' : 27,
+    },
+    mode_double: {
+    'Nokari' : 33,
+    'itsRed_v2' : 26,
+    'Étienne C🌀' : 8,
+    },
 };
