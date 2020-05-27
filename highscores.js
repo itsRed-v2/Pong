6509e4fb2f1bb1e79e04ff9bb46e2e1f695a2749
Etienne Charignon <etienne.charignon@gmail.com>
2020-05-27 22:22:05 +0200

    premier version avec badges

--- highscores.js
+++ highscores.js
@@ -1,18 +1,18 @@
 module.exports = {
-    mode_plus: {
-    'Nokari' : 107,
-    'itsRed_v2' : 92,
-    'nijou_' : 13,
-    'Céline Leroy' : 10,
-    'Étienne C🌀' : 7,
-    },
-    mode_moins: {
-    'Nokari' : 50,
-    'itsRed_v2' : 27,
-    },
-    mode_double: {
-    'Nokari' : 33,
-    'itsRed_v2' : 26,
-    'Étienne C🌀' : 8,
-    },
+mode_plus: {
+'Nokari' : 107,
+'itsRed_v2' : 92,
+'nijou_' : 13,
+'Céline Leroy' : 10,
+'Étienne C🌀' : 7,
+},
+mode_moins: {
+'Nokari' : 50,
+'itsRed_v2' : 27,
+},
+mode_double: {
+'itsRed_v2' : 34,
+'Nokari' : 33,
+'Étienne C🌀' : 8,
+},
 };
--- index.js
+++ index.js
@@ -5,34 +5,57 @@ const bot = new Discord.Client()
 const allHighscores = require('./highscores.js');
 console.log(allHighscores);
 
-const MODES = {
-  mode_plus: 'Mode Addition',
-  mode_moins: 'Mode Soustraction',
-  mode_double: 'Mode Double'
-};
-
 bot.on('ready', function () {
   console.log(`Logged in as ${bot.user.tag}!`);
 })
 
 bot.login(require('./token.js'))
 
+exitHook(() => {
+  bot.destroy();
+});
+
+const MODES = {
+  mode_plus: 'Mode Addition',
+  mode_moins: 'Mode Soustraction',
+  mode_double: 'Mode Double'
+};
+
+//================================ fonctions VV
+
+function estPremier (username,mode) {
+  var highscoresTriees = trieHighscores(allHighscores[mode]);
+  console.log (highscoresTriees[0][0])
+  return username == highscoresTriees[0][0];
+}
+
+function trouveDeuxieme (mode) {
+  var highscoresTriees = trieHighscores(allHighscores[mode]);
+  console.log (highscoresTriees[1][0])
+  return highscoresTriees[1][0];
+}
+
+function pauseQuestion (message, partie) {
+  partie.tireAuSortDeuxNombres();
+  message.reply('pong ' + partie.question() + ' !'); 
+}
+
 function printMode(mode) {
   return MODES[mode];
 }
 
 function printHighscores(allHighscores) {
-  var contenu = '\n';
+  var contenu = '';
   for (var mode in allHighscores) {
-    contenu += '`**' + printMode(mode) + '**\n`';
+    contenu += '**' + printMode(mode) + '**\n';
     var highscoresTriees = trieHighscores(allHighscores[mode]);
     for (var index in highscoresTriees) {
       var highscore = highscoresTriees[index];
       var position = parseInt(index) + 1;
-      contenu += position + ") " +  highscore[1]  + " : " + highscore[0] +"\n";
+      contenu += '`' + position + ") " +  highscore[1]  + " : " + highscore[0] + '`' +"\n";
     }
   }
-  return '`' + contenu + '`';
+  return contenu;
 }
 
 function contenuHighscores(highscores) {
@@ -70,11 +93,7 @@ function trieHighscores(highscores) {
   return sortable;
 }
 
-exitHook(() => {
-  bot.destroy();
-});
-
-//==================
+//================================ classes VV
 class Partie {
   constructor(mode) {
     this.N1;
@@ -108,7 +127,7 @@ class Partie {
     this.points++;
   }
 }
-//=========================
+//===========
 
 class Joueur {
   constructor() {
@@ -121,15 +140,9 @@ class Joueur {
     pauseQuestion(message, this.partie);
   }
 }
-
-//=========================
+//================================
 
 var joueurs = {};
-
-function pauseQuestion (message, partie) {
-  partie.tireAuSortDeuxNombres();
-  message.reply('pong ' + partie.question() + ' !'); 
-}
 
 bot.on('message', message => {
 
@@ -159,36 +172,51 @@ bot.on('message', message => {
 
     if (contenu === 'ping') {                                            // ping
       if (partie) {                                                      // ping déja une partie
-        message.reply('Une partie est déjà en cours, tu as ' + partie.score() + ` et la question est: ` + partie.question() + `
-Pour commencer une nouvelle partie, tape "ping nouveau"`)
+        message.reply('Une partie en ' + printMode(partie.mode) + ' est déjà en cours, tu as ' + partie.score() + ` et la question est: ` + partie.question() + `
+Pour commencer une nouvelle partie, tu dois d'abord perdre celle là!`)
       }
       else {  
         joueur.demarrerPartie(message);
       }
     }
+
     else if (partie) {
-      if (contenu === 'ping ' + partie.reponse()) {                    // test bonne réponse
+      if (contenu === 'ping ' + partie.reponse()) {                      // test bonne réponse
         partie.marqueUnPoint();
         message.reply('Correct ! Tu as ' + partie.score() + (partie.points > highscore ? ' **Meilleur score!**':''));
         pauseQuestion(message, partie);
-        if (partie.points > highscore) {                               // maj highscore
+        if (partie.points > highscore) {                                 // maj highscore
           allHighscores[joueur.mode][message.author.username] = partie.points;
           enregistreHighScore(allHighscores);
+          if (estPremier(message.author.username,joueur.mode)) {
+            var member = message.guild.members.cache.get(message.author.id);
+            
+            var role = {};
+            if (joueur.mode = 'mode_plus') {
+              role = message.guild.roles.cache.get('695325035392663612');
+            }
+            else if (joueur.mode = 'mode_moins') {
+              role = message.guild.roles.cache.get('696057173423423589');
+            }
+            else {
+              role = message.guild.roles.cache.get('696057317204033567');
+            }
+            member.roles.add(role).catch(console.error);
+            var deuxieme = trouveDeuxieme(joueur.mode);
+            var deuxiemeMembre = Array.from(message.guild.members.cache.values()).find(member => member.user.username == deuxieme);
+            deuxiemeMembre.roles.remove(role).catch(console.error);
+          }
         }
       }
       else if (contenu.match(/^ping -?\d+$/)) {                          // test mauvaise réponse
         message.reply("pong : Faux ! Ton score final est de " + partie.score() + (partie.points > highscore ? ", c'est ton **meilleur score!**":''));
         joueur.partie = undefined;
       }
-
-      else if (contenu === 'ping ?') {                                 // ping ?
+      else if (contenu === 'ping ?') {                                   // ping ?
         message.reply('pong ' + partie.question() + ' ! (' + printMode(partie.mode) + ')')
       }
-
-      else if (contenu === 'ping nouveau') {                           // ping nouveau
-        joueur.demarrerPartie(message);
-      }
     }
+
     else if (contenu.match(/^ping \d+$/) || contenu === 'ping ?') {      // réponse ou  "ping ?" mais aucune partie en cours
       message.reply('Aucune partie en cours. Tape "ping" pour lancer une partie')
     }
@@ -198,26 +226,30 @@ Pour commencer une nouvelle partie, tape "ping nouveau"`)
 bot.on('message', message => {
   var contenu = message.content.toLowerCase()
   if (contenu == 'ping règles') {                                        // ping règles
-    message.reply(`écris "ping" pour commencer une partie.
+    message.channel.send(`Écris "ping" pour commencer une partie.
 Je vais alors te poser une question. Réponds par 'ping <reponse>'
 Si ta réponse est correcte tu gagne un point et je te pose une nouvelle question.
 Si c'est faux, je te donne ton score et la partie se termine.
 La difficulté augmente en fonction du nombre de points.
-Une partie commence avec un mode, et elle garde ce mode jusqu'a la fin de cette partie.
-C'est évident, la calculette est interdite ^^
+Une partie commence avec un mode, et elle garde ce mode jusqu'a la fin.
 Écris "ping help" pour la liste des commandes`)
   }
   if (contenu === 'ping help') {                                         // ping help
-    message.reply(`Liste des commandes:
+    message.channel.send(`Liste des commandes:
 **ping règles** donne les règles du jeu
 **ping ?** repose la question en cours
-**ping nouveau** recommence une nouvelle partie et abandonne l'ancienne
 **ping** commence une partie (si aucune partie n'est en cours)
 **ping help** affiche cette liste
 **ping highscores** affiche les meilleurs scores des joueurs
 **ping mode <signe(s)>** choisir le mode de jeu parmi +, -, ou double (+ et -)`)
   }
   if (contenu === 'ping highscores') {
-    message.reply(printHighscores(allHighscores));
+    message.channel.send(printHighscores(allHighscores));
+  }
+  if (contenu === 'test') {
+    //console.log (message.guild.members.cache.get(message.author.id).roles.cache);
+    //console.log (message.guild.roles)                                   // pour avoir la liste des roles
+    //deuxieme ('mode_moins')
+    let membreItsRedV2 = Array.from(message.guild.members.cache.values()).find(member => member.user.username == 'itsRed_v2');
   }
 });
