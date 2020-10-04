9ece99c6d0f1340b867c80f2d67cd10ed2193653
Etienne Charignon <etienne.charignon@gmail.com>
2020-10-04 10:06:46 +0200

    cryptage Ascii XOR

--- src/pong.js
+++ src/pong.js
@@ -43,12 +43,7 @@ const CODES = [
 function toNumber(lettres) {
     var nombres = [];
     for (index = 0; index < lettres.length; index++) {
-        if (CODES.indexOf(lettres[index]) != -1) {
-            nombres[index] = CODES.indexOf(lettres[index])
-        }
-        else {
-            nombres[index] = lettres[index]
-        }
+        nombres[index] = CODES.indexOf(lettres[index])
     }
     return nombres
 }
@@ -57,75 +52,40 @@ function toNumber(lettres) {
 function toString(nombres) {
     var output = ''
     for (index = 0; index < nombres.length; index++) {
-      if (!isNaN(nombres[index])) {
         output += CODES[nombres[index]]
-      } 
-      else {
-        output += nombres[index];
-      }
     }
     return output
 }
 
-function code(nombres, cle) {
-    var nombres_codes = []
-    var precedent = -1
-    var premier_chiffre_trouve = false;
-    for (var i = 0; !premier_chiffre_trouve && i < nombres.length; i++, precedent++){
-        if (isNaN(nombres[i])) {
-            nombres_codes[i] = nombres[i];
-        }
-        else {
-            nombres_codes[i] = ((nombres[i] + 1) * cle) % CODES_LENGTH
-            premier_chiffre_trouve = true
-        }
+function code(message, cle) {
+    var messageCode = ''
+    var index = 0
+    for (index = 0; index < 5; index++) {
+        var messageAscii = message.charCodeAt(index)
+        var cleAscii = cle.charCodeAt(index)
+        var messageCodeAscii = messageAscii ^ cleAscii
+        var Code = String.fromCharCode(messageCodeAscii)
+        messageCode += Code
     }
-    for (i = precedent + 1; i < nombres.length; i++) {
-        if (!isNaN(nombres[i])) {
-            nombres_codes[i] = ((nombres[i] + 1) * (nombres_codes[precedent]+1)) % CODES_LENGTH
-            precedent = i
-        }
-        else {
-            nombres_codes[i] = nombres[i]
-        }
-    }
-    return nombres_codes
+
+    
+
+    console.log(messageCode)
+    return messageCode
 }
 
-function decode(nombres,cle) {
-    var nombres_decodes = []
-    for (index = nombres.length - 1; index >= 0; index -= 1) {
-        if (nombres[index-1] === undefined) {
-            for (i = 1; i<100;i++) {
-                //console.log((nombres[index]+CODES_LENGTH*i)/cle)
-                if (Number.isInteger((nombres[index]+CODES_LENGTH*i)/cle)%CODES_LENGTH) {
-                    nombres[index] = ((nombres[index]+CODES_LENGTH*i)/cle)%CODES_LENGTH
-                    i = 100
-                    //console.log("found")
-                }
-                //console.log(i)
-                if (i == 99) {
-                    //console.log("not found")
-                }
-            }
-        } else {
-            if (!isNaN(nombres[index])) {
-                for (i = 1; i < 100; i++) {
-                    if (Number.isInteger((nombres[index]+CODES_LENGTH*i)/nombres[index-1])) {
-                        nombres[index] = (nombres[index]+CODES_LENGTH*i)/nombres[index-1]
-                        nombres[index] = nombres[index]%CODES_LENGTH
-                        i = 100
-                        //console.log("1found")
-                        //console.log(nombres[index])
-                    }
-                    if (i == 99) {
-                        //console.log("not found")
-                    }
-                }
-            }
-        }
-    }
-    return nombres_decodes
+//console.log(String.fromCharCode(messagecodeAscii));
+
+function decode(messagecode,cle) {
+    
+    return message
 }
 
 module.exports = { toNumber, toString, code, decode, CODES_LENGTH }
+/*
+var ch = 'A';
+var index = 0;
+ 
+var i = ch.charCodeAt(index);
+console.log(i);
+*/
